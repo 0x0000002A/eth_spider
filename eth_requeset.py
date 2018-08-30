ba2c66e47726922606402529ac54b5e26b472a0e
0x0000002A <42168213+0x0000002A@users.noreply.github.com>
2018-08-30 16:39:02 +0800

    fix: remove unsupportable demo file
    remove eth_request.py since python 2 are no longer supported

--- eth_requeset.py
+++
@@ -1,5 +0,0 @@
-import urllib2
-response = urllib2.urlopen("http://api.etherscan.io/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&sort=asc")
-
-the_page = response.read()  
-print the_page
